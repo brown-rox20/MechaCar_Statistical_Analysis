942ea7109c3129d07f5b3ab8ca8b8816442911e2
Roxane Brown <roxahumada83@gmail.com>
2022-04-10 16:39:34 -0700

    add t-tests for deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -17,3 +17,15 @@ suspension_coil_data <- read.csv(file='./Suspension_Coil.csv',check.names=F,stri
 total_summary <- suspension_coil_data %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), Num_Coil=n(), .groups='keep')
 
 lot_summary <- suspension_coil_data  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), Num_Coil=n(), .groups ='keep')
+
+# Deliverable 3: T-Tests on Suspension Coils
+
+t.test(suspension_coil_data$PSI, mu=1500)
+
+lot1 <- subset(suspension_coil_data, Manufacturing_Lot=="Lot1")
+lot2 <- subset(suspension_coil_data, Manufacturing_Lot=="Lot2")
+lot3 <- subset(suspension_coil_data, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI, mu=1500)
+t.test(lot2$PSI, mu=1500)
+t.test(lot3$PSI, mu=1500)
