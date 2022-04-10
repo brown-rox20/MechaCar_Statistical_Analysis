35963ebcd8fd27c9b4385c5097a990930042681b
Roxane Brown <roxahumada83@gmail.com>
2022-04-10 14:18:19 -0700

    add RScript to calculate linear regression of mpg

---
+++ MechaCarChallenge.R
@@ -0,0 +1,8 @@
+library(dplyr)
+library(tidyverse)
+
+mecha_car_mpg <- read.csv(file='./MechaCar_mpg.csv', check.names=F, stringsAsFactors=F)
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_car_mpg)
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_car_mpg)) 
